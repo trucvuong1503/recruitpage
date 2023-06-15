f9dfff3fa822be7f482a8c9a4278648d8fe03eb7
gips-vuong <trucvuong1503@gmail.com>
2023-06-15 11:57:29 +0900

    initial commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+
 function showInfo(i) {
     var infoContents = document.getElementsByClassName("info-content");
     var tabButtons = document.getElementsByClassName("tab-button");
@@ -15,6 +15,25 @@ function sendEmail() {
     window.location.href = "mailto:example@example.com?subject=Job Application&body=Your message here";
 }
 
-})
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('scroll', function() {
+        let header = document.querySelector('.news-header');
+        let h1 = header.querySelector('h1');
+        let subheading = header.querySelector('.subheading');
+        let announcement = header.querySelector('.announcement');
+
+        let headerPos = header.getBoundingClientRect().top;
+        let windowH = window.innerHeight / 1.3;
+      
+        if (headerPos < windowH) {
+          h1.classList.add('show1');
+          subheading.classList.add('show1');
+          announcement.classList.add('show1');
+        }
+      });
+      
+});
+
+
 
 
