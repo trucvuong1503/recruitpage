389d0ab977e57bec04f9e9b904110bb2bd48db33
gips-vuong <trucvuong1503@gmail.com>
2023-06-16 17:56:17 +0900

    initial commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,64 +1,84 @@
 
+// recruitサイトの機能
 function showInfo(i) {
-    var infoContents = document.getElementsByClassName("info-content");
-    var tabButtons = document.getElementsByClassName("tab-button");
-    for (var j = 0; j < infoContents.length; j++) {
-        infoContents[j].style.display = "none";  // hide all info contents
-        tabButtons[j].classList.remove("clicked");
-    }
-    document.getElementById("info" + i).style.display = "block";
-    document.getElementById("button" + i).classList.add("clicked");  // show the selected info content
+  var infoContents = document.getElementsByClassName("info-content");
+  var tabButtons = document.getElementsByClassName("tab-button");
+  for (var j = 0; j < infoContents.length; j++) {
+      infoContents[j].style.display = "none"; 
+      tabButtons[j].classList.remove("clicked");
+  }
+  document.getElementById("info" + i).style.display = "block";
+  document.getElementById("button" + i).classList.add("clicked");
 }
-
+// メーラー機能
 
 function sendEmail() {
-    window.location.href = "mailto:example@example.com?subject=Job Application&body=Your message here";
+  let body = `
+  日発株式会社
+  採用担当　様
+  内容
+  ex)簡単な自己紹介・応募職種・問い合わせなど
+  ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+  「氏名」
+  「連絡先」
+  「メールアドレス」などの署名
+  ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝`;
+  body = encodeURIComponent(body);
+
+  let subject = `応募に関する問合せ`;
+  subject = encodeURIComponent(subject);
+  window.location.href = `mailto:saiyo@hihatu.com?subject=${subject}&body=${body}`;
 }
 
+// ヘッダーの機能
 document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener('scroll', function() {
-        let header = document.querySelector('.news-header');
-        let h1 = header.querySelector('h1');
-        let subheading = header.querySelector('.subheading');
-        let announcement = header.querySelector('.announcement');
+  window.addEventListener('scroll', function() {
+      let header = document.querySelector('.news-header');
+      let h1 = header.querySelector('h1');
+      let subheading = header.querySelector('.subheading');
+      let announcement = header.querySelector('.announcement');
 
-        let headerPos = header.getBoundingClientRect().top;
-        let windowH = window.innerHeight / 1.3;
-      
-        if (headerPos < windowH) {
-          h1.classList.add('show1');
-          subheading.classList.add('show1');
-          announcement.classList.add('show1');
-        }
-      });
-
-
-      document.getElementById("nav-icon").addEventListener("click", function() {
-        var nav = document.querySelector(".nav-menu");
-        var logo = document.querySelector(".logo");
-        var icon = document.getElementById("nav-icon");
-        if (nav.style.display === "none") {
-          nav.style.display = "block";
-          nav.classList.add("active");
-          logo.style.position = "absolute";
-          logo.style.top = "0";
-          icon.style.display = "none";
-        } else {
-          nav.style.display = "none";
-          nav.classList.remove("active");
-          logo.style.position = "relative";
-          icon.style.display = "block";
-        }
+      let headerPos = header.getBoundingClientRect().top;
+      let windowH = window.innerHeight / 1.3;
+    
+      if (headerPos < windowH) {
+        h1.classList.add('show1');
+        subheading.classList.add('show1');
+        announcement.classList.add('show1');
+      }
     });
-        const menuToggle = document.querySelector('.menu-toggle');
-        const navMenu = document.querySelector('.nav-menu');
 
-        document.addEventListener('DOMContentLoaded', (event) => {
-        menuToggle.addEventListener('click', function() {
-            navMenu.classList.toggle('active');
-            menuToggle.classList.toggle('open');
-       
-    })
-    })
-     
+
+    const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const navLinks = document.querySelectorAll('.nav-links');  // Selecting all the nav links
+
+function closeMenu() {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show1");
+    body.classList.remove("disabledScroll");
+}
+
+menuBtn.onclick = () => {
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+    cancelBtn.classList.add("show1");
+    body.classList.add("disabledScroll");
+}
+
+cancelBtn.onclick = closeMenu;
+
+navLinks.forEach(link => {
+    link.onclick = closeMenu;
+})
+
+window.onscroll = () => {
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+   
 })
